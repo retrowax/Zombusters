94305f2fd3047beb8d6a795b1af64e91df1b243a
Ferran Pons <ferranpons@gmail.com>
2017-12-07 20:38:21 +0100

    Added changelog

--- travis/release.sh
+++ travis/release.sh
@@ -3,4 +3,4 @@ echo "Beta Deploy"
 zip -r zombusters_windows.zip /home/travis/build/retrowax/Zombusters/ZombustersWindows/bin/Windows/Release/
 ~/bin/butler -V
 ~/bin/butler login
-~/bin/butler push zombusters_windows.zip retrowax/zombusters:windows
+~/bin/butler push zombusters_windows.zip retrowax/zombusters:windows --userversion $MAJOR.$MINOR.$PATCH
