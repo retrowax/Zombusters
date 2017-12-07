b87f003339dcefd43d2c70719478c7679acf4f30
Ferran Pons <ferranpons@gmail.com>
2017-12-07 20:06:54 +0100

    version changed and url to beta details changed

--- travis/tweetAfterDeploy.sh
+++ travis/tweetAfterDeploy.sh
@@ -3,5 +3,5 @@
 if [[ $TRAVIS_TAG ]]
   python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New Zombusters version $MAJOR.$MINOR.$PATCH now available for download. Check out the details at https://retrowax.itch.io/zombusters"
 else
-  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New Zombusters BETA version $MAJOR.$MINOR.$PATCH now available for download. Check out the details at https://retrowax.itch.io/zombusters"
+  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New Zombusters BETA version $MAJOR.$MINOR.$PATCH now available for download. Check out the details at https://github.com/retrowax/Zombusters/releases"
 fi
