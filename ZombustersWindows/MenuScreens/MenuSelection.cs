adf1c7d17d0db8e852fdb10c68177e0454e26bf0
Ferran Pons <ferranpons@gmail.com>
2019-12-08 17:27:56 +0100

    Removed unneeded options on main menu and gameplay menu (#31)

---
+++ ZombustersWindows/MenuScreens/MenuSelection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZombustersWindows
+{
+    // Used by the Menu event handlers to get the menu selection.
+    public class MenuSelection : EventArgs
+    {
+        private int m_selection;
+
+        public MenuSelection(int s) {
+            m_selection = s;
+        }
+
+        public int Selection {
+            get { return m_selection; }
+            set { m_selection = value; }
+        }
+    }
+}
