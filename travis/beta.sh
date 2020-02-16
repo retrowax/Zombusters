3a9cfc453ac4254335758607857568e92d1c4bfd
Ferran Pons <ferranpons@gmail.com>
2020-02-16 18:05:37 +0100

    [ISSUE #75] Correct deploy path

--- travis/beta.sh
+++ travis/beta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Beta Deploy"
-zip -r zombusters-windows-$SNAPSHOT_NOW.zip /home/travis/build/retrowax/Zombusters/ZombustersWindows/bin/Windows/Release/
+zip -r zombusters-windows-$SNAPSHOT_NOW.zip /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/
 ~/bin/butler -V
 ~/bin/butler login
 ~/bin/butler push zombusters-windows-$SNAPSHOT_NOW.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW
