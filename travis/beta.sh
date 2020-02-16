abec25dfc62afefcef270dc74a304ca9195505e8
Ferran Pons <ferranpons@gmail.com>
2020-02-16 17:46:06 +0100

    [ISSUE #75] Added Snapshots when merging a PR

--- travis/beta.sh
+++ travis/beta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Beta Deploy"
-zip -r zombusters_windows.zip /home/travis/build/retrowax/Zombusters/ZombustersWindows/bin/Windows/Release/
+zip -r zombusters-windows-$SNAPSHOT_NOW.zip /home/travis/build/retrowax/Zombusters/ZombustersWindows/bin/Windows/Release/
 ~/bin/butler -V
 ~/bin/butler login
-~/bin/butler push zombusters_windows.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH
+~/bin/butler push zombusters-windows-$SNAPSHOT_NOW.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW
