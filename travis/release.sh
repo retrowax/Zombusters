a3fd977d8fad25adbae1e4f2d639866412845430
Ferran Pons <ferranpons@gmail.com>
2020-02-16 18:26:40 +0100

    [ISSUE #75] Remove file before upload it again

--- travis/release.sh
+++ travis/release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Beta Deploy"
-zip -r zombusters-windows.zip /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/
 ~/bin/butler -V
 ~/bin/butler login
+~/bin/butler wipe zombusters-windows.zip
+zip -r zombusters-windows.zip /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/
 ~/bin/butler push zombusters-windows.zip retrowax/zombusters:windows --userversion $MAJOR.$MINOR.$PATCH
