75e02951a7cf06d470135f4eec95918ee1d32262
Ferran Pons <ferranpons@gmail.com>
2020-03-07 13:45:23 +0100

    [ISSUE #79] Corrected generated zip path

--- travis/release.sh
+++ travis/release.sh
@@ -3,5 +3,6 @@ echo "Beta Deploy"
 ~/bin/butler -V
 ~/bin/butler login
 ~/bin/butler wipe zombusters-windows.zip
-zip -r zombusters-windows.zip /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/
+mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/ ./ZombustersWindows
+zip -r zombusters-windows.zip /ZombustersWindows/
 ~/bin/butler push zombusters-windows.zip retrowax/zombusters:windows --userversion $MAJOR.$MINOR.$PATCH
