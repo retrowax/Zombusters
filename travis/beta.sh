ab0870d6eb8c6ca085157786fb9e3148873552ad
Ferran Pons <ferranpons@gmail.com>
2020-03-07 14:16:36 +0100

    [ISSUE #79] Changed Beta

--- travis/beta.sh
+++ travis/beta.sh
@@ -3,5 +3,6 @@ echo "Beta Deploy"
 ~/bin/butler -V
 ~/bin/butler login
 ~/bin/butler wipe zombusters-windows-beta.zip
-zip -r zombusters-windows-beta.zip /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/
+mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/ ./ZombustersWindows
+zip -r zombusters-windows.zip /ZombustersWindows/
 ~/bin/butler push zombusters-windows-beta.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW
