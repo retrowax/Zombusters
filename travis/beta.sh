ef8b0fb26a0f3c0e6147d0e02105299291a3802d
Ferran Pons <ferranpons@gmail.com>
2020-03-10 16:23:23 +0100

    solve deploying beta issue

--- travis/beta.sh
+++ travis/beta.sh
@@ -4,5 +4,5 @@ echo "Beta Deploy"
 ~/bin/butler login
 ~/bin/butler wipe zombusters-windows-beta.zip
 mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/ ./ZombustersWindows
-zip -r zombusters-windows.zip /ZombustersWindows/
+zip -r zombusters-windows.zip ./ZombustersWindows/
 ~/bin/butler push zombusters-windows-beta.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW
--- travis/release.sh
+++ travis/release.sh
@@ -4,5 +4,5 @@ echo "Beta Deploy"
 ~/bin/butler login
 ~/bin/butler wipe zombusters-windows.zip
 mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/ ./ZombustersWindows
-zip -r zombusters-windows.zip /ZombustersWindows/
+zip -r zombusters-windows.zip ./ZombustersWindows/
 ~/bin/butler push zombusters-windows.zip retrowax/zombusters:windows --userversion $MAJOR.$MINOR.$PATCH
