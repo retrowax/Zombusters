54b475ff6f53e24ed129f5e419997f5de407b1e9
Ferran Pons <ferranpons@gmail.com>
2020-03-10 16:57:53 +0100

    Solved issue wiith zip (#84)

--- travis/beta.sh
+++ travis/beta.sh
@@ -3,6 +3,7 @@ echo "Beta Deploy"
 ~/bin/butler -V
 ~/bin/butler login
 ~/bin/butler wipe zombusters-windows-beta.zip
-mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release/ ./ZombustersWindows
-zip -r zombusters-windows-beta.zip ./ZombustersWindows/
+mkdir ZombustersWindows
+mv /home/travis/build/retrowax/Zombusters-Enhanced-Edition/ZombustersWindows/bin/Windows/Release ./ZombustersWindows
+zip -r -v zombusters-windows-beta.zip ./ZombustersWindows
 ~/bin/butler push zombusters-windows-beta.zip retrowax/zombusters:windows-beta --userversion $MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW
