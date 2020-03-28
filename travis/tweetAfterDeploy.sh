2845fab3728b71569ac6bfa1307900b100863d3b
Ferran Pons <ferranpons@gmail.com>
2020-03-28 11:46:00 +0100

    updated the tweet message after deploy

--- travis/tweetAfterDeploy.sh
+++ travis/tweetAfterDeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $TRAVIS_TAG ]; then
-  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New Zombusters version $MAJOR.$MINOR.$PATCH now available for download. Check out the details at https://retrowax.itch.io/zombusters"
+  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New #Zombusters version $MAJOR.$MINOR.$PATCH available! Click the link to #download it. Enjoy! #opensource #itchio #macos #linux #windows #free https://retrowax.itch.io/zombusters"
 else
-  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "New Zombusters BETA version $MAJOR.$MINOR.$PATCH now available for download. Check out the details at https://github.com/retrowax/Zombusters/blob/master/CHANGELOG.md"
+  python ./twitterUpdate.py -k "$TWITTER_API_KEY" -s "$TWITTER_API_SECRET" -a "$TWITTER_ACCESS_TOKEN" -t "$TWITTER_ACCESS_TOKEN_SECRET" -u "#Zombusters BETA v$MAJOR.$MINOR.$PATCH-$SNAPSHOT_NOW available for #download now. #opensource #itchio #macos #linux #windows #free Check out the details at https://github.com/retrowax/Zombusters-Enhanced-Edition/commits/master"
 fi
